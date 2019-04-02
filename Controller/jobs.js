af584fefe3e41d5317e0465accdfb432d8351092
Rakib Uddin <16103030@iubat.edu>
2019-04-02 23:40:00 +0600

    Controller Cleanup

--- Controller/jobs.js
+++ Controller/jobs.js
@@ -5,7 +5,6 @@ const Job = require("../Models/Jobs")
 const db = require("../Config/database");
 
 // Routes
-
 // Landing Page
 exp_router.get("/" , (req, res) => {
     Job.findAll()
@@ -33,27 +32,10 @@ exp_router.get("/contact" , (req, res) => {
     res.render("contact")
 })
 
-// Tesing DB
-exp_router.get("/add" , (req, res) => {
-    const data = {
-        title : "Looking for a React Developer",
-        technologies : "React, javascript, html,css",
-        budget: "3000$",
-        discription : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        contact_email : "16103030@iubat.edu"
-    }
-    let {title, technologies , budget , discription , contact_email} = data
-    // Job.
-    //     create({
-    //         title, 
-    //         technologies , 
-    //         budget ,
-    //         discription , 
-    //         contact_email
-    //     })
-    //     .then(jobs => {res.redirect("/"); console.log(jobs)})
-    //     .catch(err => {console.log(err)})
+// Post Add
+exp_router.post("/add" , (req, res) => {
 })
+
 
 // Exports
 module.exports = exp_router
